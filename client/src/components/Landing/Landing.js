68d50491aa0c642ae52cc98ed6a47d0478de9b74
Aanchal Singh <aanchalsinghuk@gmail.com>
2024-03-23 22:11:08 +0530

    commit

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import logo from "./1.png";
+import l1 from "./2.png";
+import l2 from "./3.png";
 import "./Landing.css";
 
 function Landing() {
@@ -15,9 +17,22 @@ function Landing() {
                         <button className="btn btn-dark mr-4">Shop Now</button>
                         <button className="btn btn-light">Learn More</button>
                     </div >
-                    <div className="flex mt-2">
-                        <div className="bg-pink-600 rounded mr-4" style={{ height: "10vh", width: "20vw" }}></div>
-                        <div className="bg-yellow-600 rounded" style={{ height: "10vh", width: "20vw" }}></div>
+                    <div className="photo flex mt-2">
+                        <div>
+                            <img className="mr-4" src={l1} alt="" />
+                            <div className='flex justify-between'>
+                                <p className='mt-2 ml-4 text-blue-400 '>Sweater</p>
+                                <p className='mt-2 text-green-600 mr-5'>$95.00</p>
+                            </div>
+
+                        </div>
+                        <div>
+                            <img className="" src={l2} alt="" />
+                            <div className='flex justify-between'>
+                                <p className='mt-2 ml-4 text-pink-400 '>Jacket</p>
+                                <p className='mt-2 mr-4 text-green-600 '>$55.00</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <img src={logo} alt="" style={{ height: "80vh", width: "auto" }} />
