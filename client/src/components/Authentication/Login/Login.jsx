41ef86476c6e912d01794f4d5ab1aa8b4f1f80e6
Aanchal Singh <aanchalsinghuk@gmail.com>
2024-03-24 15:51:37 +0530

    commit

--- client/src/components/Authentication/Login/Login.jsx
+++ client/src/components/Authentication/Login/Login.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./Login.css";
 
 const Login = ({ setLoginUser }) => {
@@ -8,9 +8,11 @@ const Login = ({ setLoginUser }) => {
   const [user, setUser] = useState({
     email: "",
     password: "",
+    fullName: "",
+    username: "",
   });
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isActive, setIsActive] = useState(false);
+  const [containerClass, setContainerClass] = useState("");
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -36,74 +38,107 @@ const Login = ({ setLoginUser }) => {
     }
   };
 
-  const toggleLogin = () => {
-    setIsLoggedIn(!isLoggedIn);
-  };
-
-  const toggleActive = () => {
-    setIsActive(true);
+  const toggleForm = (formType) => {
+    setIsActive(false);
+    setContainerClass(formType === "login" ? "active" : "log-in");
+    setUser({
+      email: "",
+      password: "",
+      fullName: "",
+      username: "",
+    });
   };
 
   return (
-    <div className={`container-form ${isLoggedIn ? "log-in" : ""}`}>
-      <div className="container-info">
-        <div className="info-item">
-          <div className="table">
-            <div className="table-cell">
-              <p>Have an account?</p>
-              <div className="btn" onClick={toggleLogin}>
-                Log in
+    <div className={`login-form-body ${containerClass}`}>
+      <div className={`login-form-container ${containerClass}`}>
+        <div className={`box ${isActive ? "active" : ""}`}></div>
+        <div className="container-forms">
+          <div className="container-info">
+            <div className="info-item">
+              <div className="table">
+                <div className="table-cell">
+                  <p>Have an account?</p>
+                  <div className="btn" onClick={() => toggleForm("login")}>
+                    Log in
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-        <div className="info-item">
-          <div className="table">
-            <div className="table-cell">
-              <p>Don't have an account?</p>
-              <div className="btn" onClick={toggleLogin}>
-                Sign up
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="container-forms">
-        <div className="container-form">
-          <div className={`form-item ${isLoggedIn ? "log-in" : "sign-up"}`}>
-            <div className="table">
-              <div className="table-cell">
-                <input
-                  name="email"
-                  placeholder="Email"
-                  value={user.email}
-                  onChange={handleChange}
-                  required
-                  type="email"
-                />
-                <input
-                  name="password"
-                  placeholder="Password"
-                  type="Password"
-                  value={user.password}
-                  onChange={handleChange}
-                  required
-                />
-                <div className="btn" onClick={toggleActive}>
-                  Log in
+            <div className="info-item">
+              <div className="table">
+                <div className="table-cell">
+                  <p>Don't have an account?</p>
+                  <div className="btn" onClick={() => toggleForm("signup")}>
+                    Sign up
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-          <div className={`form-item ${isLoggedIn ? "sign-up" : "log-in"}`}>
-            <div className="table">
-              <div className="table-cell">
-                <input name="email" placeholder="Email" type="text" />
-                <input name="fullName" placeholder="Full Name" type="text" />
-                <input name="Username" placeholder="Username" type="text" />
-                <input name="Password" placeholder="Password" type="Password" />
-                <div className="btn" onClick={toggleActive}>
-                  Sign up
+          <div
+            className={`container-form ${
+              containerClass === "log-in" ? "log-in" : ""
+            }`}
+          >
+            <div className="form-item">
+              <div className="table">
+                <div className="table-cell">
+                  {containerClass !== "log-in" ? (
+                    <>
+                      <input
+                        name="email"
+                        placeholder="Email"
+                        type="text"
+                        value={user.email}
+                        onChange={handleChange}
+                      />
+                      <input
+                        name="password"
+                        placeholder="Password"
+                        type="password"
+                        value={user.password}
+                        onChange={handleChange}
+                      />
+                      <div className="btn" onClick={login}>
+                        Log in
+                      </div>
+                    </>
+                  ) : (
+                    <>
+                      <input
+                        name="fullName"
+                        placeholder="Full Name"
+                        type="text"
+                        value={user.fullName}
+                        onChange={handleChange}
+                      />
+                      <input
+                        name="username"
+                        placeholder="Username"
+                        type="text"
+                        value={user.username}
+                        onChange={handleChange}
+                      />
+                      <input
+                        name="email"
+                        placeholder="Email"
+                        type="text"
+                        value={user.email}
+                        onChange={handleChange}
+                      />
+                      <input
+                        name="password"
+                        placeholder="Password"
+                        type="password"
+                        value={user.password}
+                        onChange={handleChange}
+                      />
+                      <div className="btn" onClick={login}>
+                        Sign up
+                      </div>
+                    </>
+                  )}
                 </div>
               </div>
             </div>
