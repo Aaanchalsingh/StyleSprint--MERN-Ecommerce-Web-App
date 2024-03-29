35d22334e27128363a20f167f350c939135a2311
Aanchal Singh <aanchalsinghuk@gmail.com>
2024-03-29 10:41:31 +0530

    Final

--- client/src/components/Item/Item.js
+++ client/src/components/Item/Item.js
@@ -2,10 +2,9 @@ import ItemCarousel from './Carousel/ItemCarousel';
 import Description from './Description/Description';
 import Detail from './Detail/Detail';
 import './Item.css';
-import Related from './Related/Related';
 
 const Item = (props) => {
-    return ( 
+    return (
         <div className="item__container">
             <div className="detail__and__carousel__container">
                 <ItemCarousel item={props.item}/>
@@ -14,11 +13,8 @@ const Item = (props) => {
             <div className="item__description__container">
                 <Description item={props.item}/>
             </div>
-            <div className="related__items__container">
-                <Related category={props.item.category}/>
-            </div>
         </div>
      );
 }
- 
+
 export default Item;
