90c92f97c9ddb02c951a883fc2724d3af5e55e87
Aanchal Singh <aanchalsinghuk@gmail.com>
2024-04-04 20:14:07 +0530

    commit

--- client/src/components/Nav/Nav-Brand/Navbrand.js
+++ client/src/components/Nav/Nav-Brand/Navbrand.js
@@ -5,7 +5,7 @@ import Logo from "./style.png";
 const NavBrand = () => {
     return (
         <div href="#home" className='navbrand__container mt-2'>
-           <h1 className='navbrand'>
+           <h1 className='navbrand pl-12'>
                <Link to="/"><img src={Logo} alt="" srcset="" /></Link>
             </h1>
         </div>
